80d6483dec5936c95f5314b31498374a491e6c65
benitakannout <94390863+benitakannout@users.noreply.github.com>
2022-01-09 14:45:32 +0200

    Filled in data to repositories

--- Baigiamasis Darbas (Studentu Dienynas)/Repozitorijos/PazymiuRepozitorija.cs
+++ Baigiamasis Darbas (Studentu Dienynas)/Repozitorijos/PazymiuRepozitorija.cs
@@ -8,5 +8,70 @@ namespace Baigiamasis_Darbas__Studentu_Dienynas_.Repozitorijos
 {
     public class PazymiuRepozitorija
     {
+        public List<Pazymiai> pazymiai = new List<Pazymiai>();
+        public PazymiuRepozitorija()
+        {
+            pazymiai.Add(new Pazymiai(1, "Sandra", 1, 7));
+            pazymiai.Add(new Pazymiai(1, "Sandra", 2, 9));
+            pazymiai.Add(new Pazymiai(1, "Sandra", 3, 7));
+            pazymiai.Add(new Pazymiai(1, "Sandra", 1, 8));
+            pazymiai.Add(new Pazymiai(2, "Aurimas", 1, 9));
+            pazymiai.Add(new Pazymiai(2, "Aurimas", 2, 8));
+            pazymiai.Add(new Pazymiai(2, "Aurimas", 3, 9));
+            pazymiai.Add(new Pazymiai(2, "Aurimas", 4, 9));
+            pazymiai.Add(new Pazymiai(3, "Karolis", 1, 6));
+            pazymiai.Add(new Pazymiai(3, "Karolis", 2, 10));
+            pazymiai.Add(new Pazymiai(3, "Karolis", 3, 8));
+            pazymiai.Add(new Pazymiai(3, "Karolis", 4, 8));
+            pazymiai.Add(new Pazymiai(4, "Brigita", 1, 10));
+            pazymiai.Add(new Pazymiai(4, "Brigita", 2, 9));
+            pazymiai.Add(new Pazymiai(4, "Brigita", 3, 10));
+            pazymiai.Add(new Pazymiai(4, "Brigita", 4, 10));
+            pazymiai.Add(new Pazymiai(5, "Laurynas", 1, 7));
+            pazymiai.Add(new Pazymiai(5, "Laurynas", 2, 7));
+            pazymiai.Add(new Pazymiai(5, "Laurynas", 3, 6));
+            pazymiai.Add(new Pazymiai(5, "Laurynas", 4, 7));
+            pazymiai.Add(new Pazymiai(6, "Sabina", 1, 8));
+            pazymiai.Add(new Pazymiai(6, "Sabina", 2, 8));
+            pazymiai.Add(new Pazymiai(6, "Sabina", 3, 8));
+            pazymiai.Add(new Pazymiai(6, "Sabina", 4, 8));
+            pazymiai.Add(new Pazymiai(7, "Asta", 1, 10));
+            pazymiai.Add(new Pazymiai(7, "Asta", 2, 9));
+            pazymiai.Add(new Pazymiai(7, "Asta", 3, 9));
+            pazymiai.Add(new Pazymiai(7, "Asta", 4, 9));
+            pazymiai.Add(new Pazymiai(8, "Gabrielius", 1, 7));
+            pazymiai.Add(new Pazymiai(8, "Gabrielius", 2, 8));
+            pazymiai.Add(new Pazymiai(8, "Gabrielius", 3, 6));
+            pazymiai.Add(new Pazymiai(8, "Gabrielius", 4, 7));
+            pazymiai.Add(new Pazymiai(9, "Rokas", 1, 10));
+            pazymiai.Add(new Pazymiai(9, "Rokas", 2, 10));
+            pazymiai.Add(new Pazymiai(9, "Rokas", 3, 10));
+            pazymiai.Add(new Pazymiai(9, "Rokas", 4, 10));
+            pazymiai.Add(new Pazymiai(10, "Simona", 1, 8));
+            pazymiai.Add(new Pazymiai(10, "Simona", 2, 6));
+            pazymiai.Add(new Pazymiai(10, "Simona", 3, 8));
+            pazymiai.Add(new Pazymiai(10, "Simona", 4, 7));
+            pazymiai.Add(new Pazymiai(11, "Ieva", 1, 6));
+            pazymiai.Add(new Pazymiai(11, "Ieva", 2, 8));
+            pazymiai.Add(new Pazymiai(11, "Ieva", 3, 9));
+            pazymiai.Add(new Pazymiai(11, "Ieva", 4, 8));
+            pazymiai.Add(new Pazymiai(12, "Paulius", 1, 7));
+            pazymiai.Add(new Pazymiai(12, "Paulius", 2, 6));
+            pazymiai.Add(new Pazymiai(12, "Paulius", 3, 6));
+            pazymiai.Add(new Pazymiai(12, "Paulius", 4, 6));
+            pazymiai.Add(new Pazymiai(13, "Tomas", 1, 9));
+            pazymiai.Add(new Pazymiai(13, "Tomas", 2, 9));
+            pazymiai.Add(new Pazymiai(13, "Tomas", 3, 9));
+            pazymiai.Add(new Pazymiai(13, "Tomas", 4, 9));
+            pazymiai.Add(new Pazymiai(14, "Tadas", 1, 10));
+            pazymiai.Add(new Pazymiai(14, "Tadas", 2, 7));
+            pazymiai.Add(new Pazymiai(14, "Tadas", 3, 7));
+            pazymiai.Add(new Pazymiai(14, "Tadas", 4, 8));
+        }
+
+        public List<Pazymiai> Retrieve()
+        {
+            return pazymiai;
+        }
     }
 }
--- Baigiamasis Darbas (Studentu Dienynas)/Repozitorijos/StudentuRepozitorija.cs
+++ Baigiamasis Darbas (Studentu Dienynas)/Repozitorijos/StudentuRepozitorija.cs
@@ -8,5 +8,34 @@ namespace Baigiamasis_Darbas__Studentu_Dienynas_.Repozitorijos
 {
     public class StudentuRepozitorija
     {
+        public List<Studentai> studentai = new List<Studentai>();
+
+        public StudentuRepozitorija()
+        {
+            studentai.Add(new Studentai(1, "Sandra"));
+            studentai.Add(new Studentai(2, "Aurimas"));
+            studentai.Add(new Studentai(3, "Karolis"));
+            studentai.Add(new Studentai(4, "Brigita"));
+            studentai.Add(new Studentai(5, "Laurynas"));
+            studentai.Add(new Studentai(6, "Sabina"));
+            studentai.Add(new Studentai(7, "Asta"));
+            studentai.Add(new Studentai(8, "Gabrielius"));
+            studentai.Add(new Studentai(9, "Rokas"));
+            studentai.Add(new Studentai(10, "Simona"));
+            studentai.Add(new Studentai(11, "Ieva"));
+            studentai.Add(new Studentai(12, "Paulius"));
+            studentai.Add(new Studentai(13, "Tomas"));
+            studentai.Add(new Studentai(14, "Tadas"));
+        }
+        public Studentai Retrieve(string vardas)
+        {
+            var studentas = studentai.FirstOrDefault(x => x.Vardas == vardas);
+            return studentas;
+        }
+
+        public List<Studentai> Retrieve()
+        {
+            return studentai;
+        }
     }
 }
