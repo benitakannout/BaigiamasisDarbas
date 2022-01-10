c077d5fdc6447519c106215a6beaadf7ac33e9ba
benitakannout <94390863+benitakannout@users.noreply.github.com>
2022-01-10 19:01:28 +0200

    Fixed data, added new methods

--- Baigiamasis Darbas (Studentu Dienynas)/Pazymiai.cs
+++ Baigiamasis Darbas (Studentu Dienynas)/Pazymiai.cs
@@ -10,17 +10,15 @@ namespace Baigiamasis_Darbas__Studentu_Dienynas_
     public class Pazymiai
     {
         public int Id { get; set; }
-        public string Vardas { get; set; }
         public int Trimestras { get; set; }
         public int Pazymys { get; set; }
         public Pazymiai()
         {
 
         }
-        public Pazymiai(int id, string vardas, int trimestras, int pazymys)
+        public Pazymiai(int id, int trimestras, int pazymys)
         {
             Id = id;
-            Vardas = vardas;
             Trimestras = trimestras;
             Pazymys = pazymys;
         }
